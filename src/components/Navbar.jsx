3fe74cc41e1c427915f1afd4f5df3243a8f30719
boxianglin <boxiang.lin@wsu.edu>
2023-04-23 22:48:13 -0700

    page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,11 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>塞班软件</h1>
       <ul className='hidden md:flex'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Company</li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
+        <li className='p-5'>HOME</li>
+        <li className='p-5'>服务</li>
+        <li className='p-4'>联系</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
